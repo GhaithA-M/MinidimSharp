64abf181a3940ba221d29c18c49b3698afae244e
Ghaith Al-Maliki <122114790+GhaithA-M@users.noreply.github.com>
2023-12-11 18:09:41 +0100

    Update main.yml

---
+++ deploy.bat
@@ -0,0 +1,26 @@
+@echo off
+SET /P versionNumber="Enter the new version number (e.g., 0.2): "
+
+REM Check if version number is provided
+IF "%versionNumber%"=="" (
+    echo Version number is required.
+    exit /b 1
+)
+
+REM Set tag name
+SET tagName=v%versionNumber%
+
+REM Commit changes
+git add .
+SET /P commitMessage="Enter your commit message: "
+git commit -m "%commitMessage%"
+
+REM Create a new tag
+git tag -a %tagName% -m "Release %tagName%"
+
+REM Push changes and tag
+git push origin main
+git push origin %tagName%
+
+echo Deployment and tagging complete.
+pause
